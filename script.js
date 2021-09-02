5619aa19e59ed2a54b407eb29794b71a22e4e4f8
Diana <89095339+Dianapirs@users.noreply.github.com>
2021-09-02 21:11:20 +0300

    Third

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function calculations(a, b, c) {
     resultF = (a - b) / c;
 }
 calculations(24, 3, 3);
-console.log(resultF);
+console.log('Результат вычислений равен ' + resultF);
 
 
 //Second
@@ -15,3 +15,17 @@ function calculations2(d) {
 }
 calculations2(2);
 console.log('Число в квадрате равно ' + resultS2 + ',' + ' число в кубе равно ' + resultS3);
+
+//Third
+console.log('Third');
+function minmax (e, f) {
+    if (e < f) {
+        console.log('Меньшим числом является ' + e + ', ' + 'большим числом является ' + f);
+    } else if(e > f) {
+        console.log('Меньшим числом является ' + f + ', ' + 'большим числом является ' + e);
+    } else {
+        console.log('Числа равны');
+    }
+}
+minmax(6, 8);
+
