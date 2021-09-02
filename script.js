648f2b7bf385441d1f5fe6fee3498f3d41ad8e20
Diana <89095339+Dianapirs@users.noreply.github.com>
2021-09-02 21:02:44 +0300

    Second

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 //First
 console.log('First');
-function compute(a, b, c) {
+function calculations(a, b, c) {
     resultF = (a - b) / c;
 }
-compute(24, 3, 3);
+calculations(24, 3, 3);
 console.log(resultF);
+
+
+//Second
+console.log('Second');
+function calculations2(d) {
+    resultS2 = Math.pow(3, 2);;
+    resultS3 = Math.pow(3, 3);
+}
+calculations2(2);
+console.log('Число в квадрате равно ' + resultS2 + ',' + ' число в кубе равно ' + resultS3);
